86fb245ccdea80a25bac131c7d836fb52c003b39
zqzal <936952020@qq.com>
2020-02-17 22:00:55 +0800

    网络请求add

--- lib/common/net_cache.dart
+++ lib/common/net_cache.dart
@@ -33,11 +33,70 @@ class NetCache extends Interceptor{
   @override
   Future onRequest(RequestOptions options) async {
     if (!Global.profile.cache.enable) return options;
-
-
-
-    // TODO: implement onRequest
-    return super.onRequest(options);
+    // refresh标记是否是"下拉刷新"
+    bool refresh = options.extra["refresh"] == true;
+    //如果是下拉刷新，先删除相关缓存
+    if (refresh) {
+      if (options.extra["list"] == true) {
+        //若是列表，则只要url中包含当前path的缓存全部删除（简单实现，并不精准）
+        cache.removeWhere((key, v) => key.contains(options.path));
+      } else {
+        // 如果不是列表，则只删除uri相同的缓存
+        delete(options.uri.toString());
+      }
+      return options;
+    }
+    if (options.extra["noCache"] != true &&
+        options.method.toLowerCase() == 'get') {
+      String key = options.extra["cacheKey"] ?? options.uri.toString();
+      var ob = cache[key];
+      if (ob != null) {
+        //若缓存未过期，则返回缓存内容
+        if ((DateTime.now().millisecondsSinceEpoch - ob.timeStamp) / 1000 <
+            Global.profile.cache.maxAge) {
+          return cache[key].response;
+        } else {
+          //若已过期则删除缓存，继续向服务器请求
+          cache.remove(key);
+        }
+      }
+    }
   }
+
+  @override
+  Future onError(DioError err) async{
+    // TODO: implement onError
+    //错误状态下不缓存
+  }
+
+  @override
+  Future onResponse(Response response) {
+    // TODO: implement onResponse
+    //如果启用缓存，将返回结果保存到缓存
+    if(Global.profile.cache.enable){
+      _saveCache(response);
+    }
+  }
+
+  void delete(String key) {
+    cache.remove(key);
+  }
+
+  _saveCache(Response object) {
+    RequestOptions options = object.request;
+    if(options.extra["noCache"] != true && options.method.toLowerCase() == "get"){
+      //如果缓存数量超过最大数量限制，则先移除最早的一条记录
+      if(cache.length == Global.profile.cache.maxCount){
+        cache.remove(cache[cache.keys.first]);
+      }
+      String key = options.extra["cacheKey"] ?? options.uri.toString();
+      cache[key] = CacheObject(object);
+    }
+  }
+///refresh  bool 如果为true，则本次请求不使用缓存，但新的请求结果依然会被缓存
+///noCache  bool 本次请亲禁用缓存，请求结果也不会被缓存
+
+
+
 
 }
