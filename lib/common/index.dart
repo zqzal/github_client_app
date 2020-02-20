165eb0f075d1b8d4d1b16f7413db03f960e9052d
zqzal <936952020@qq.com>
2020-02-20 16:46:09 +0800

    login

--- lib/common/index.dart
+++ lib/common/index.dart
@@ -1,3 +1,4 @@
 export 'git_api.dart';
 export 'global.dart';
 export 'net_cache.dart';
+export 'funs.dart';
--- lib/routes/home_page.dart
+++ lib/routes/home_page.dart
@@ -9,16 +9,33 @@ class HomeRoute extends StatefulWidget{
 }
 
 class _HomeRouteState extends State<HomeRoute>{
-
-
-
+  
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
+        //GmLocalizations是我们提供的一个Localizations类
+        //用于支持多语言，因此当APP语言改变时，凡是使用GmLocalizations
+        //动态获取的文案都会是相应语言的文案
         title: Text(GmLocalizations.of(context).home),
       ),
+      body: _buildBody(), //构建主页面
     );
   }
+
+  Widget _buildBody() {
+    UserModel userModel = Provider.of(context);
+    if(!userModel.isLogin){
+      //用户未登录，显示登录按钮
+      return Center(
+        child: RaisedButton(
+          child: Text(GmLocalizations.of(context).login),
+          onPressed: () => Navigator.of(context).pushNamed("login"),
+        ),
+      );
+    }
+  }
+
+
 }
